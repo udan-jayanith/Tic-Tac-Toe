07afc3d4c627707926da2a90779f3af842f38d87
udanjayanith <j.udanjayanith@gmail.com>
2024-10-08 18:22:19 +0530

    gg

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
 let map = ['', '', '', '', '', '', '', '', '']
 const turn = document.querySelector('.turn')
-let clickCounter = 0
 let xORo = 'x'
 let counter = 0
+let validator = true
+const tapEffect = new Audio('sound Effects/click.mp3')
+
+function audioToPlay(audio) {
+	audio
+		.play()
+		.then(() => {
+			document.querySelector('.dialog-backdrop').style.display = 'none'
+		})
+		.catch(() => {
+			const dialog = document.querySelector('.dialog')
+			dialog.style.display = 'flex'
+
+			document.querySelector('.playBtn').addEventListener('click', () => {
+				tapEffect.play()
+				audio.play()
+
+				dialog.style.display = 'none'
+				document.querySelector('.dialog-backdrop').style.display = 'none'
+			})
+		})
+}
 
 function checkConditions() {
 	if (map[0] != '' && map[0] == map[4] && map[4] == map[8]) {
@@ -36,9 +57,6 @@ function checkConditions() {
 	}
 }
 
-let validator = true
-const gameOver = new Audio('sound Effects/game-over.mp3')
-
 function finishMatch(index1, index2, index3, player, status) {
 	validator = false
 	document.querySelectorAll('.cell').forEach((el, i) => {
@@ -55,6 +73,8 @@ function finishMatch(index1, index2, index3, player, status) {
 		})
 		turn.innerText = `Tie`
 	}
+
+	const gameOver = new Audio('sound Effects/game-over.mp3')
 	gameOver.play()
 
 	const ResetGameBtn = document.querySelector('.reset-game-btn')
@@ -75,29 +95,15 @@ function finishMatch(index1, index2, index3, player, status) {
 		`
 		validator = true
 		turn.innerText = "X's Turn"
-		clickCounter = 0
 		xORo = 'x'
 		ResetGameBtn.style.display = 'none'
 		counter = 0
 	})
 }
 
-function audioToPlay(audio) {
-	audio
-		.play()
-		.then(() => {
-			document.querySelector('.dialog-backdrop').style.display = 'none'
-		})
-		.catch(() => {
-			const audioPermission = document.querySelector('.audio-permission')
-			audioPermission.style.display = 'flex'
-		})
-}
-
-const tapEffect = new Audio('sound Effects/click.mp3')
 const backgroundMusic = new Audio('sound Effects/Background-music.mp3')
-
 audioToPlay(backgroundMusic)
+
 backgroundMusic.volume = 0.16
 backgroundMusic.addEventListener('ended', function () {
 	backgroundMusic.currentTime = 0 // Reset the audio to the beginning
@@ -132,12 +138,4 @@ board.onclick = function (event) {
 	checkConditions()
 }
 
-document.querySelector('.playBtn').addEventListener('click', () => {
-	tapEffect.play()
 
-	const audioPermission = document.querySelector('.audio-permission')
-	audioPermission.style.display = 'none'
-	backgroundMusic.play()
-
-	document.querySelector('.dialog-backdrop').style.display = 'none'
-})
